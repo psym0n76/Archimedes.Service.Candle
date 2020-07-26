a8a87b4fbd3ac5855823a25473f9c958980b2356
Simon Burton <burt76@outlook.com>
2020-07-26 17:37:00 +0100

    logging change

--- Archimedes.Service.Candle/Http/MarketClient.cs
+++ Archimedes.Service.Candle/Http/MarketClient.cs
@@ -14,28 +14,23 @@ namespace Archimedes.Service.Candle.Http
     public class MarketClient : IMarketClient
     {
         private readonly HttpClient _client;
-        //private const string RequestUri = "market";
-        //private readonly Config _config;
-        private readonly ILogger<CandleRequestManager> _logger;
+        private readonly ILogger<MarketClient> _logger;
 
-        public MarketClient(HttpClient httpClient, IOptions<Config> config, ILogger<CandleRequestManager> logger)
+        public MarketClient(HttpClient httpClient, IOptions<Config> config, ILogger<MarketClient> logger)
         {
             httpClient.BaseAddress = new Uri($"{config.Value.ApiRepositoryUrl}");
             httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
             _client = httpClient;
             _logger = logger;
-            //_config = config.Value;
         }
 
         public async Task<IEnumerable<MarketDto>> GetMarketAsync(CancellationToken ct = default)
         {
-            //var request = new HttpRequestMessage(HttpMethod.Get, RequestUri);
-            //var response = await _client.SendAsync(request, ct);
             var response = await _client.GetAsync($"market",ct);
 
             if (!response.IsSuccessStatusCode)
             {
-                _logger.LogWarning("No Markets Returned");
+                _logger.LogWarning($"Failed to Get {response.ReasonPhrase} from {_client.BaseAddress}/market");
                 return Array.Empty<MarketDto>();
             }
 

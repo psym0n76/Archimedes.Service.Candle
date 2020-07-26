ff285db06a087d76f65b62b715b200f16a8b40a3
Simon Burton <burt76@outlook.com>
2020-07-26 17:05:19 +0100

    alighed http request

--- Archimedes.Service.Candle/Http/MarketClient.cs
+++ Archimedes.Service.Candle/Http/MarketClient.cs
@@ -14,19 +14,21 @@ namespace Archimedes.Service.Candle.Http
     {
         private readonly HttpClient _client;
         private const string RequestUri = "market";
+        private readonly Config _config;
 
         public MarketClient(HttpClient httpClient, IOptions<Config> config)
         {
             httpClient.BaseAddress = new Uri($"{config.Value.ApiRepositoryUrl}");
             httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
             _client = httpClient;
+            _config = config.Value;
         }
 
         public async Task<IEnumerable<MarketDto>> GetMarketAsync(CancellationToken ct = default)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, RequestUri);
-
-            var response = await _client.SendAsync(request, ct);
+            //var request = new HttpRequestMessage(HttpMethod.Get, RequestUri);
+            //var response = await _client.SendAsync(request, ct);
+            var response = await _client.GetAsync($"{_config.ApiRepositoryUrl}/market",ct);
 
             if (!response.IsSuccessStatusCode)
             {
